c3512cbabc82343038d0df60a95e46574fb642ff
Kavandeep <kavan1@live.co.uk>
2022-07-20 14:27:33 +0100

    style changes applied

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Counter.scss";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const changeCount = (symbol) => {
-    if (symbol === "+") {
+  const updateCount = (plusOrMinus) => {
+    if (plusOrMinus === "+") {
       setCount(count + 1);
-    } else if (symbol === "-" && count > 0) {
+    } else if (plusOrMinus === "-" && count > 0) {
       setCount(count - 1);
     } else return count === 0;
   };
 
   return (
     <div className="counter">
-      <p className="counter__minus" onClick={() => changeCount("-")}>
+      <p className="counter__minus" onClick={() => updateCount("-")}>
         -
       </p>
       <p className="counter__number">{count}</p>
-      <p className="counter__plus" onClick={() => changeCount("+")}>
+      <p className="counter__plus" onClick={() => updateCount("+")}>
         +
       </p>
     </div>
